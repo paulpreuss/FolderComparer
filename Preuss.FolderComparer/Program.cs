b8a75e3e8921a6d804a1b526384444d4293fbdfa
Paul Preuß <pauljonas.preuss@icloud.com>
2023-10-27 17:33:06 +0200

    set up project

---
+++ Preuss.FolderComparer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        
+    }).Build();
+
+await builder.RunAsync();
