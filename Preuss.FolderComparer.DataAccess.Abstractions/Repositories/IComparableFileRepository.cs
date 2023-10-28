c8a66afb207f1c041e636fa594df29dd59fa49ce
Paul Preuß <pauljonas.preuss@icloud.com>
2023-10-28 08:03:09 +0200

    implemented file import

--- Preuss.FolderComparer.DataAccess.Abstractions/Repositories/IComparableFileRepository.cs
+++ Preuss.FolderComparer.DataAccess.Abstractions/Repositories/IComparableFileRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace Preuss.FolderComparer.DataAccess.Abstractions.Repositories;
+﻿using Preuss.FolderComparer.DataAccess.Abstractions.DataClasses;
+
+namespace Preuss.FolderComparer.DataAccess.Abstractions.Repositories;
 
 public interface IComparableFileRepository
 {
-
+    IEnumerable<ComparableFile> GetFiles(string path);
 }
--- Preuss.FolderComparer.DataAccess/Repositories/ComparableFileRepository.cs
+++ Preuss.FolderComparer.DataAccess/Repositories/ComparableFileRepository.cs
@@ -1,7 +1,70 @@
-﻿using Preuss.FolderComparer.DataAccess.Abstractions.Repositories;
+﻿using Preuss.FolderComparer.DataAccess.Abstractions.DataClasses;
+using Preuss.FolderComparer.DataAccess.Abstractions.Repositories;
+using System.IO.Abstractions;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Preuss.FolderComparer.DataAccess.Repositories;
 
 public class ComparableFileRepository : IComparableFileRepository
 {
+    private readonly IFileSystem _fileSystem;
+
+    public ComparableFileRepository(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+
+    public IEnumerable<ComparableFile> GetFiles(string path)
+    {
+        var files = new List<ComparableFile>();
+
+        var filesInCurrentDirectory = _fileSystem.Directory.GetFiles(path);
+        files.AddRange(GetComparableFiles(filesInCurrentDirectory));
+
+        var directoriesInCurrentDirectory = _fileSystem.Directory.GetDirectories(path);
+        if (directoriesInCurrentDirectory is not null 
+            && directoriesInCurrentDirectory.Any())
+        {
+            foreach (var directory in directoriesInCurrentDirectory)
+            {
+                files.AddRange(GetFiles(directory));
+            }
+        }
+
+        return files;
+    }
+
+    private IEnumerable<ComparableFile> GetComparableFiles(string[]? paths)
+    {
+        var files = new List<ComparableFile>();
+
+        if (paths is null || paths.Length < 1) return files;
+
+        foreach ( var path in paths)
+        {
+            files.Add(GetComparableFile(path));
+        }
+
+        return files;
+    }
+
+    private ComparableFile GetComparableFile(string path)
+    {
+        var file = new ComparableFile();
+
+        var fileContent = _fileSystem.File.ReadAllText(path);
+
+        file.Hash = ComputeHash(fileContent);
+        file.Path = path;
+
+        return file;
+    }
+
+    private string ComputeHash(string fileContent)
+    {
+        var hash = MD5.HashData(Encoding.UTF8.GetBytes(fileContent));
+
+        return BitConverter.ToString(hash).Replace("-", "");
+    }
 }
